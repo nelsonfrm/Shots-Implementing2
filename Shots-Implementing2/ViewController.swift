e600c8eeb759854d5fb7af9771cb9014cc16c0a8
nelsonfrm <nmarteleira@gmail.com>
2014-10-11 10:14:17 +0100

    Initial Commit

---
+++ Shots-Implementing2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shots-Implementing2
+//
+//  Created by Nelson on 11/10/14.
+//  Copyright (c) 2014 Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
