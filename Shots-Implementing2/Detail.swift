d038f133b0ddbe16491f79bbfddeb1b277446b4b
nelsonfrm <nmarteleira@gmail.com>
2014-10-11 19:05:04 +0100

    implementing basic animations
    Animate transition home to detail

---
+++ Shots-Implementing2/Detail.swift
@@ -0,0 +1,25 @@
+//
+//  Detail.swift
+//  Shots-Implementing2
+//
+//  Created by Nelson on 11/10/14.
+//  Copyright (c) 2014 Nelson. All rights reserved.
+//
+
+import UIKit
+
+class Detail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // Change the Status Bar Color
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+
+
+}
--- Shots-Implementing2/Home.swift
+++ Shots-Implementing2/Home.swift
@@ -38,6 +38,12 @@ class Home: UIViewController {
     
     @IBAction func ShareButtonDidPress(sender: AnyObject) {
         ShareView.hidden = false
+        ShareView.alpha = 0
+        
+        UIView.animateWithDuration(0.5, animations: {
+            self.ShareView.alpha = 1
+        
+        })
         
     }
     
@@ -46,14 +52,32 @@ class Home: UIViewController {
     }
     
     @IBAction func ImageButtonDidPress(sender: AnyObject) {
-    
+   
+        UIView.animateWithDuration(0.7, animations: {
+            self.DialogView.frame = CGRectMake(0, 0, 320, 568)
+            self.DialogView.layer.cornerRadius = 0
+            self.ImageButton.frame = CGRectMake(0, 0, 320, 240)
+            self.LikeButton.alpha = 0
+            self.ShareButton.alpha = 0
+            self.userButton.alpha = 0
+            self.HeaderView.alpha = 0
+        
+       
+            },completion: { finished in
+                self.performSegueWithIdentifier("HomeToDetail", sender: self)
+            
+        })
     }
-    
+    // Change the Status Bar Color
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+         return UIStatusBarStyle.LightContent
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //Doing blur
         insertBlurView(BackgroundMaskView, UIBlurEffectStyle.Dark)
         insertBlurView(HeaderView, UIBlurEffectStyle.Dark)
         // Do any additional setup after loading the view.
